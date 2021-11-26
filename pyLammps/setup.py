1809c207394a945e5555b7abeb103c8260316289
atripodo <62422098+atripodo@users.noreply.github.com>
2021-11-26 17:51:58 +0100

    Update setup.py

--- pyLammps/setup.py
+++ pyLammps/setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1.1',
     author='Antonio Tripodo and Gianfranco Cordella',
     author_email='a.tripodo90@gmail.com',
-    install_requires=['numpy', 'scipy','pandas'],
+    install_requires=['numpy', 'scipy','pandas','freud-analysis','pyvoro'],
     packages=['pyLammps'],
 )

eb3dd9e47f146ecd9fc145ecfedc1419c8ade834
atripodo <62422098+atripodo@users.noreply.github.com>
2021-11-26 17:51:29 +0100

    Update setup.py

--- pyLammps/setup.py
+++ pyLammps/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pyLammps',
     version='0.1.1',
-    author='Antonio Tripodo',
+    author='Antonio Tripodo and Gianfranco Cordella',
     author_email='a.tripodo90@gmail.com',
     install_requires=['numpy', 'scipy','pandas'],
     packages=['pyLammps'],
